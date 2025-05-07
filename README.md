28dc21c89e6cc4ba0b7dd3dd56f2c27f560b6d54
GroovieGermanikus <groovie@mango.markets>
2025-05-07 10:49:15 +0200

    readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Usage
+
+Add to **Cargo.toml**:
+```
+rust-soft-assert = { git = "https://github.com/grooviegermanikus/rust-soft-assert.git", tag = "v0.1.0" }
+```
+
+```rust
+soft_assert!(false, "This is a soft assert");
+```
--- src/soft_assert.rs
+++ src/soft_assert.rs
@@ -1,9 +1,8 @@
-
 #[macro_export]
 macro_rules! soft_assert {
     ($cond:expr $(,)?) => {
         if !$cond {
-            log::warn!(
+            log::error!(
                 "Soft assert failed: {}\nStack trace:\n{}",
                 stringify!($cond),
                 std::backtrace::Backtrace::capture().to_string()
@@ -13,7 +12,7 @@ macro_rules! soft_assert {
     ($cond:expr, $($arg:tt)+) => {
         if !$cond {
 
-            log::warn!(
+            log::error!(
                 "Soft assert failed: {}\nStack trace:\n{}",
                 format_args!($($arg)+),
                 std::backtrace::Backtrace::capture().to_string()
@@ -22,13 +21,16 @@ macro_rules! soft_assert {
     };
 }
 
-
-
 #[test]
 fn test_soft() {
     let _ = tracing_subscriber::fmt::fmt().try_init();
 
     soft_assert!(false, "My fuzzy text");
-    soft_assert!(false, "My fuzzy text with args: {} {}", 42, "foo");
+}
 
+#[test]
+fn test_soft_with_args() {
+    let _ = tracing_subscriber::fmt::fmt().try_init();
+
+    soft_assert!(false, "My fuzzy text with args: {} {}", 42, "foo");
 }
