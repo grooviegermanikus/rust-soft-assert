3c66ae30ce6389db2d3f38d6ec8185b8a2ab7a93
GroovieGermanikus <groovie@mango.markets>
2025-05-07 09:53:22 +0200

    initial

---
+++ src/lib.rs
@@ -0,0 +1,1 @@
+mod soft_assert;
---
+++ src/soft_assert.rs
@@ -0,0 +1,32 @@
+
+#[macro_export]
+macro_rules! soft_assert {
+    ($cond:expr $(,)?) => {
+        if !$cond {
+            log::warn!(
+                "Soft assert failed: {}\nStack trace:\n{:?}",
+                stringify!($cond),
+                std::backtrace::Backtrace::capture()
+            );
+        }
+    };
+    ($cond:expr, $($arg:tt)+) => {
+        if !$cond {
+            log::warn!(
+                "Soft assert failed: {}\nStack trace:\n{:?}",
+                format_args!($($arg)+),
+                std::backtrace::Backtrace::capture()
+            );
+        }
+    };
+}
+
+
+#[test]
+fn test_soft() {
+    tracing_subscriber::fmt::fmt().init();
+    
+    soft_assert!(false, "This is a soft assert");
+
+
+}
