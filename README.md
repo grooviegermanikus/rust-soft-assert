6952a9b60fca9090cd76a7cc57c808096b5c9e44
GroovieGermanikus <groovie@mango.markets>
2025-05-07 10:54:38 +0200

    add _eq + _ne

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Usage
 
+`Defensive Programming Pattern` to allow to check for conditions without panicking in production code (i.e. not in tests).
+
 Add to **Cargo.toml**:
 ```
-rust-soft-assert = { git = "https://github.com/grooviegermanikus/rust-soft-assert.git", tag = "v0.1.0" }
+rust-soft-assert = { git = "https://github.com/grooviegermanikus/rust-soft-assert.git", tag = "v0.2" }
 ```
 
 ```rust
--- src/soft_assert.rs
+++ src/soft_assert.rs
@@ -21,6 +21,56 @@ macro_rules! soft_assert {
     };
 }
 
+#[macro_export]
+macro_rules! soft_assert_eq {
+    ($left:expr, $right:expr $(,)?) => {
+        if $left != $right {
+            log::error!(
+                "Soft assert failed: {} != {}\nStack trace:\n{}",
+                stringify!($left),
+                stringify!($right),
+                std::backtrace::Backtrace::capture().to_string()
+            );
+        }
+    };
+    ($left:expr, $right:expr, $($arg:tt)+) => {
+        if $left != $right {
+            log::error!(
+                "Soft assert failed: {} != {}\nMessage: {}\nStack trace:\n{}",
+                stringify!($left),
+                stringify!($right),
+                format_args!($($arg)+),
+                std::backtrace::Backtrace::capture().to_string()
+            );
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! soft_assert_ne {
+    ($left:expr, $right:expr $(,)?) => {
+        if $left == $right {
+            log::error!(
+                "Soft assert failed: {} == {}\nStack trace:\n{}",
+                stringify!($left),
+                stringify!($right),
+                std::backtrace::Backtrace::capture().to_string()
+            );
+        }
+    };
+    ($left:expr, $right:expr, $($arg:tt)+) => {
+        if $left == $right {
+            log::error!(
+                "Soft assert failed: {} == {}\nMessage: {}\nStack trace:\n{}",
+                stringify!($left),
+                stringify!($right),
+                format_args!($($arg)+),
+                std::backtrace::Backtrace::capture().to_string()
+            );
+        }
+    };
+}
+
 #[test]
 fn test_soft() {
     let _ = tracing_subscriber::fmt::fmt().try_init();
@@ -33,4 +83,26 @@ fn test_soft_with_args() {
     let _ = tracing_subscriber::fmt::fmt().try_init();
 
     soft_assert!(false, "My fuzzy text with args: {} {}", 42, "foo");
+}
+
+#[test]
+fn test_soft_assert_eq() {
+    let _ = tracing_subscriber::fmt::fmt().try_init();
+
+    // This should log an error because 1 != 2
+    soft_assert_eq!(1, 2, "Values are not equal: {} and {}", 1, 2);
+
+    // This should not log an error because 1 == 1
+    soft_assert_eq!(1, 1);
+}
+
+#[test]
+fn test_soft_assert_ne() {
+    let _ = tracing_subscriber::fmt::fmt().try_init();
+
+    // This should log an error because 1 == 1
+    soft_assert_ne!(1, 1, "Values are equal: {} and {}", 1, 1);
+
+    // This should not log an error because 1 != 2
+    soft_assert_ne!(1, 2);
 }
